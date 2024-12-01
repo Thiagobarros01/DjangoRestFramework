dfd5584fe1b94432da7ced8fc1b43e8d1608d63e
unknown <thiagosbarros_@hotmail.com>
2024-12-01 01:20:32 -0300

    alteracao na views

--- api_rest/views.py
+++ api_rest/views.py
@@ -36,42 +36,40 @@ def get_by_nick(request,nick):
         serialiezer = UserSerializer(user)
         return Response(serialiezer.data)
 
-@api_view(['GET','PUT','POST','DELETE'])
+@api_view(['GET','POST','PUT','DELETE'])
 def user_manager(request):
-
     if request.method == 'GET':
-    
-        try:
+      
+      try: 
+        if request.GET['user']:
+            user_nickname = request.GET['user']
+            try:
+                user = User.objects.get(pk=user_nickname)
+            except:
+                serialiezer = UserSerializer(user)
+                return Response(status = status.HTTP_404_NOT_FOUND)
+      
+      except:
+          return Response(status=status.HTTP_400_BAD_REQUEST)
             
-            if request.GET['user']:
-                try:
-                    user_nickname = request.GET['user']
-                    user = User.objects.get(pk=user_nickname)
-                    serialiezer = UserSerializer(user)
-                    return Response(serialiezer.data)
-                except:
-                    return Response(status=status.HTTP_404_NOT_FOUND)
-            else:
-                return Response(status=status.HTTP_400_BAD_REQUEST)
-            
-        except:
-            return Response(status=status.HTTP_400_BAD_REQUEST)       
+                
 
-          
+    return Response(serialiezer.data)
+
+
     if request.method == 'POST':
+        
+        new_user = request.data
 
-        try:
-            dados = request.data
-            serialiezer = UserSerializer(data=dados)
-            if serialiezer.is_valid():
-             serialiezer.save()      
+        serialiezer = UserSerializer(data = new_user)
+
+        if serialiezer.isvalid():
+            serialiezer.save()
             
-            return Response(status=status.HTTP_201_CREATED)
+            return Response(status = status.HTTP_400_BAD_REQUEST)
             
-        except:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-
-
+        
+       
 
 
 
